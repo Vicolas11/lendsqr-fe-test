b8f179a73a2f2632f0ba4a0a69be56dd907ea591
Vicolas11 <incrediblechamp1@gmail.com>
2023-01-02 13:14:56 +0100

    style: positioned the lendsqr logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import PageNotFound from "./pages/PageNotFound";
+import ReactDOM from "react-dom/client";
+import LoginPage from "./pages/Login";
+import "./styles/index.css";
+import React from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
