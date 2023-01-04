e8a681ce59ad66bddd052dd47c187e4bcf2fff8f
Vicolas11 <incrediblechamp1@gmail.com>
2023-01-04 15:23:58 +0100

    feat: add icons to LogoutModal component

--- src/components/LogoutModal.tsx
+++ src/components/LogoutModal.tsx
@@ -1,19 +1,23 @@
 import { hideLogoutModal } from "../store/slice/global.slice";
+import { ModalProps } from "../../interfaces/modal.interface";
 import { useAppDispatch } from "../hooks/store.hook";
 import { RiShutDownLine } from "react-icons/ri";
 import { MdClose } from "react-icons/md";
+import { Fragment, useRef } from "react";
 import { SlDocs } from "react-icons/sl";
 import { BsBell } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import BackBlurDrop from "./BackDrop";
-import { Fragment } from "react";
 
-const LogoutModal = (): JSX.Element => {
+const LogoutModal = ({ show }: ModalProps): JSX.Element => {
   const dispatch = useAppDispatch();
+  const nodeRef = useRef(null);
+
   return (
     <Fragment>
-      <BackBlurDrop show={true} exit={1000} />
-      <div className="logout_modal">
+      <BackBlurDrop show={show} exit={1000} />
+
+      <div className="logout_modal" ref={nodeRef}>
         <span className="modal_header">
           <div className="close" onClick={() => dispatch(hideLogoutModal())}>
             <MdClose className="w-8 h-8 md:h-10 pl-2 text-[#213F7D]" />
@@ -30,17 +34,17 @@ const LogoutModal = (): JSX.Element => {
           </div>
         </div>
         <div className="docs">
-            <SlDocs className="w-6 h-6 md:h-8 p-0" />
-            <h1>Docs</h1>
+          <SlDocs className="w-6 h-6 md:h-8 p-0" />
+          <h1>Docs</h1>
         </div>
         <div className="bell">
-            <BsBell className="w-6 h-6 md:h-8 p-0" />
-            <h1>Notifications</h1>
+          <BsBell className="w-6 h-6 md:h-8 p-0" />
+          <h1>Notifications</h1>
         </div>
         <div className="logout">
           <Link to={"/"}>
-              <RiShutDownLine className="w-6 h-6 md:h-8 p-0" />
-              <h1>Logout</h1>
+            <RiShutDownLine className="w-6 h-6 md:h-8 p-0" />
+            <h1>Logout</h1>
           </Link>
         </div>
       </div>
