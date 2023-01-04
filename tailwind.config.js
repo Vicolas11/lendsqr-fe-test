75862b52a144e2318d6e456a2396355919a19233
Vicolas11 <incrediblechamp1@gmail.com>
2023-01-04 15:13:38 +0100

    chore: add worksans font to fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       fontFamily: {
         custom: ["AvenirBold", "AvenirRegular"],
         roboto: ["Roboto", "sans-serif"],
+        worksans: ["WorkSansRegular"],
       },
     },
     screens: {
