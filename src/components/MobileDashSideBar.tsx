9eab911e23decf5b9c0f16052c5c78d7338fb9ec
Vicolas11 <incrediblechamp1@gmail.com>
2023-01-04 15:08:47 +0100

    feat: add MobileDashSideBar component visible in mobile screen

---
+++ src/components/MobileDashSideBar.tsx
@@ -0,0 +1,66 @@
+import { hideSideBarModal } from "../store/slice/global.slice";
+import { ModalProps } from "../../interfaces/modal.interface";
+import { CSSTransition } from "react-transition-group";
+import { useAppDispatch } from "../hooks/store.hook";
+import animate from "../styles/Animation.module.css";
+import { MdClose } from "react-icons/md";
+import { Fragment, useRef } from "react";
+import { Link } from "react-router-dom";
+import BackBlurDrop from "./BackDrop";
+import Businesses from "./Businesses";
+import Customers from "./Customers";
+import Settings from "./Settings";
+
+const MobileDashSideBar = ({ show }: ModalProps): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const nodeRef = useRef(null);
+
+  return (
+    <Fragment>
+      <BackBlurDrop show={show} exit={1000} />
+      <CSSTransition
+        in={show}
+        key={"enter"}
+        nodeRef={nodeRef}
+        timeout={400}
+        mountOnEnter
+        unmountOnExit
+        classNames={{
+          enterActive: animate.slideEnterActive,
+          exitActive: animate.slideExitActive,
+        }}
+      >
+        <aside className="mobile_dash_sidebar_wrapper" ref={nodeRef}>
+          <div className="close" onClick={() => dispatch(hideSideBarModal())}>
+            <MdClose className="w-8 h-8 md:h-10 pl-2 text-[#213F7D]" />
+          </div>
+          <div className="logo">
+            <Link to={"/"}>
+              <img src="../assets/svg/logo.svg" alt="Logo" />
+            </Link>
+          </div>
+          <span className="switch_organisation">
+            <span>
+              <img src="../assets/svg/briefcase.svg" alt="Briefcase" />
+            </span>
+            <span className="text">
+              <h3>Switch Organization</h3>
+              <img src="../assets/svg/dropdown.svg" alt="DropDown" />
+            </span>
+          </span>
+          <div className="dashboard">
+            <span>
+              <img src="../assets/svg/home.svg" alt="home" />
+            </span>
+            <h3>Dashboard</h3>
+          </div>
+          <Customers />
+          <Businesses />
+          <Settings />
+        </aside>
+      </CSSTransition>
+    </Fragment>
+  );
+};
+
+export default MobileDashSideBar;
