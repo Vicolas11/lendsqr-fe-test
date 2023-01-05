1309eb5d4d49a59d22dae147c5478c44772912c5
Vicolas11 <incrediblechamp1@gmail.com>
2023-01-05 03:37:07 +0100

    feat: removed return keyword & add id to the div

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -5,16 +5,14 @@ const Customers = (): JSX.Element => {
   return (
     <Fragment>
       <h3>customers</h3>
-      {dashData[0]?.customers?.map((data) => {
-        return (
-          <div key={`${data?.id}`} className="customers">
-            <span>
-              <img src={`../assets/svg/${data?.icon}`} alt={data?.alt} />
-            </span>
-            <h3>{data?.title}</h3>
-          </div>
-        );
-      })}
+      {dashData[0]?.customers?.map((data, idx) => (
+        <div key={`${data?.id}`} className="customers" id={`customers_${idx}`}>
+          <span>
+            <img src={`../assets/svg/${data?.icon}`} alt={data?.alt} />
+          </span>
+          <h3>{data?.title}</h3>
+        </div>
+      ))}
     </Fragment>
   );
 };
