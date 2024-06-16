11ee90711409508992c46b45f77f127d73aa79f2
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-16 06:58:34 +0100

    chore: define CustomButton component props ts interface

--- src/interfaces/props.interfaces.ts
+++ src/interfaces/props.interfaces.ts
@@ -1,5 +1,13 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface PrivateRouterProps {
   children: ReactNode;
+}
+
+export interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  xtraStyle?: string;
+  isOutline?: boolean;
+  showLoader?: boolean;
+  isDeleteBtn?: boolean;
 }
