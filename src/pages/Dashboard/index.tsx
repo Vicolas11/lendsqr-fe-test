acb44795fc907138a9fbbb09d69644bdc2fa3f8a
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-19 14:50:19 +0100

    feat: add Dashboard page with styling

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import DashboardMain from "../../components/DashboardMain";
+import DashboardLayout from "../../Layout";
+
+export default function Dashboard() {
+  return (
+    <DashboardLayout title="Dashboard">
+      <DashboardMain />
+    </DashboardLayout>
+  );
+}
