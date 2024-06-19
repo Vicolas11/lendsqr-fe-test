7a230c4d9ea5db5888c3c8c1ef06bb29debcec99
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-19 14:49:30 +0100

    chore: add typescript interfaces (hooks, status, store)

--- src/interfaces/props.interfaces.ts
+++ src/interfaces/props.interfaces.ts
@@ -4,10 +4,42 @@ export interface PrivateRouterProps {
   children: ReactNode;
 }
 
-export interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface CustomButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   xtraStyle?: string;
   isOutline?: boolean;
   showLoader?: boolean;
   isDeleteBtn?: boolean;
+}
+
+export interface Props {
+  show: boolean;
+}
+
+export interface BackDropProps {
+  show: boolean;
+  exit: number;
+}
+
+export interface PaginationProps {
+  maxNumOfPages: number;
+  currentPageNum: number;
+  increasePageNum: () => void;
+  decreasePageNum: () => void;
+}
+
+export interface LayoutProps {
+  title: string;
+  children: ReactNode;
+}
+
+export interface DropdownModalProps {
+  arrayLen: number;
+  index: number;
+}
+
+export interface OptionModalProps {
+  arrayLen: number;
+  index: number;
 }
---
+++ src/interfaces/status.interface.ts
@@ -0,0 +1,3 @@
+export interface UserStatusProps {
+  status: string | "Active" | "Inactive" | "Blacklisted" | "Pending";
+}
