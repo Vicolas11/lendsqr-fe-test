414d5a32d6e52d0c94ee7847388780dc36df78dc
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-22 17:49:46 +0100

    feat: add features (fetch data, show loader, show error message) with styling

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,44 +1,50 @@
-import { setUserId, toggleUserOption } from "../../store/slice/global.slice";
+import {
+  setHasFetched,
+  setTotalPage,
+  setTotalUsers,
+  setUserData,
+} from "../../store/slice/userdata.slice";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../../hooks/store.hook";
-import { IMockAPIData } from "../../interfaces/mockapi.interface";
-import { useCallback, useEffect, useRef, useState, MouseEvent } from "react";
-import { getUserData } from "../../store/slice/userdata.slice";
-import { userStatusRand } from "../../utils/duplicate.util";
+import { useEffect, useRef, useState, MouseEvent } from "react";
 import { constants } from "../../config/constants.config";
 import FilterDropDownModal from "../FilterDropDownModal";
 import { userStatus } from "../../utils/userstatus.util";
 import { formatDate } from "../../utils/formatime.util";
-import { dashData } from "../../data/dashboardData";
+import { columnTitle } from "../../data/dashboardData";
 import { SlOptionsVertical } from "react-icons/sl";
 import UserOptionModal from "../UserOptionModal";
 import { MdFilterList } from "react-icons/md";
+import { arrowBackIcon } from "../../assets";
 import styles from "./styles.module.scss";
 import Pagination from "../Pagination";
 import Loader from "../Loader";
 
+
 export default function Table(): JSX.Element {
-  const userOption = useAppSelector((state) => state.global.userOption);
-  const userDataArr = useAppSelector((state) => state.userData.data);
+  const {
+    searchTerm,
+    totalPages,
+    hasFetched,
+    data: userDataArr,
+    filteredData: userFilteredDataArr,
+  } = useAppSelector((state) => state.userData);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [currentPageNum, setCurrentPageNum] = useState(1);
-  const [totalNumOfUsers, setTotalNumOfUsers] = useState(
-    userDataArr.length || 0
-  );
-  const [newUserDataArr, setNewUserDataArr] = useState<IMockAPIData[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pageNumber = searchParams.get("page");
+  const isFilter = searchParams.get("isFilter");
+  const isSearch = searchParams.get("isSearch");
+  const [page, setPage] = useState(!pageNumber ? 1 : +pageNumber);
   const [hasError, setHasError] = useState({ msg: "", status: false });
   const tbodyRef = useRef<HTMLTableSectionElement>(null);
-  const trRefs = useRef<HTMLTableRowElement[]>([]);
-  const divRef = useRef<HTMLDivElement>(null);
   const { mockAPI } = constants;
-  const [top, setTop] = useState(0);
-  const numOfUsers = 10;
-  const maxNumOfPages = Math.floor(totalNumOfUsers / numOfUsers);
-  const tableData = dashData[4]?.tableData;
   const dispatch = useAppDispatch();
   const tableRef = useRef<HTMLTableElement>(null);
-
   const [clickedRow, setClickedRow] = useState<number | null>(null);
   const [clickedColumn, setClickedColumn] = useState<number | null>(null);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const pageSize = 9;
 
   const handleRowClick = (index: number) => {
     setClickedRow(clickedRow === index ? null : index);
@@ -51,94 +57,47 @@ export default function Table(): JSX.Element {
     setClickedRow(null);
   };
 
-  const columnTitle = [
-    "Organisation",
-    "Username",
-    "Email",
-    "Phone number",
-    "Date Joined",
-    "Status",
-  ];
-
-  const onOptionClick = ({ idx, id }: { idx: number; id: string }) => {
-    setTop(idx);
-    dispatch(setUserId(id));
-    dispatch(toggleUserOption());
+  const handlePageChange = (newPageNum: number) => {
+    dispatch(setHasFetched(true));
+    setPage(newPageNum);
+    setSearchParams({ page: `${newPageNum}` });
   };
-
-  const slicedUserData = useCallback(
-    (currentPageNum: number) => {
-      const startIdx = numOfUsers * (currentPageNum - 1);
-      return userDataArr.slice(startIdx, startIdx + numOfUsers);
-    },
-    [userDataArr]
-  );
-
-  const increasePageNum = () => {
-    if (maxNumOfPages === currentPageNum) {
-      return;
-    }
-    setCurrentPageNum((prev) => prev + 1);
-  };
-
-  const decreasePageNum = () => {
-    if (currentPageNum <= 1) {
-      return;
-    }
-    setCurrentPageNum((prev) => prev - 1);
-  };
-
-  // useEffect(() => {
-  //   // if (userDataArr.length === 0) {
-  //   setIsLoading(true);
-  //   fetch(mockAPI)
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       const data = json as IMockAPIData[];
-  //       console.log(data);
-  //       setTotalNumOfUsers(data.length);
-  //       dispatch(getUserData(data));
-  //       setIsLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       // const errMsg = response?.data?.errors[0];
-  //       setHasError({ msg: err?.message, status: true });
-  //       setIsLoading(false);
-  //       console.log(err?.message);
-  //     });
-  //   // }
-  // }, [dispatch, mockAPI]);
 
   useEffect(() => {
-    // Setting UserOptionModal Position relative to each tbody rows
-    if (
-      divRef.current !== null &&
-      trRefs.current[top] !== null &&
-      tbodyRef.current !== null
-    ) {
-      const parent = tbodyRef.current?.getBoundingClientRect() as DOMRect;
-      const child = trRefs.current[top]?.getBoundingClientRect() as DOMRect;
-      const { top: parentT, height: parentH } = parent;
-      const { top: childT } = child;
-      const realPosTop = parentT && childT - parentT + 35;
-      const { height: h } = divRef.current?.getBoundingClientRect() as DOMRect;
-      const isOverflow: boolean = realPosTop + h > parentH;
-      const relPosBottom = realPosTop - h - 10;
-      if (!isOverflow) {
-        divRef.current.style.top = `${realPosTop}px`;
-      } else {
-        divRef.current.style.top = `${relPosBottom}px`;
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          `${mockAPI}/data?page=${page}&pageSize=${pageSize}`
+        );
+
+        if (!response.ok) {
+          setHasError({
+            status: true,
+            msg: response.statusText || "An error occurred!",
+          });
+          setIsLoading(false);
+          return;
+        }
+
+        const data = await response.json();
+        dispatch(setUserData(data.data));
+        dispatch(setTotalUsers(data.totalRecords));
+        dispatch(setTotalPage(data.totalPages));
+        setIsLoading(false);
+      } catch (error: any) {
+        console.error("Error fetching data:", error);
+        setHasError({
+          status: true,
+          msg: error.message || "An error occurred!",
+        });
+        setIsLoading(false);
       }
-    }
+    };
 
-    // Trigger Error Message
-    const { msg } = hasError;
-    console.log(msg);
-  }, [userOption, top, hasError]);
-
-  useEffect(() => {
-    setNewUserDataArr(slicedUserData(currentPageNum));
-  }, [currentPageNum, slicedUserData]);
+    hasFetched && fetchData();
+  }, [page, hasFetched]);
 
   // Hide Modal FilterDropDownModal and UserOptionModal when clicked elsewhere
   useEffect(() => {
@@ -167,58 +126,94 @@ export default function Table(): JSX.Element {
   return (
     <div className="relative">
       <div className={styles.dashTableWrapper}>
-        {isLoading && <Loader />}
-        <table ref={tableRef}>
-          <thead>
-            <tr>
-              {columnTitle.map((title, idx) => (
-                <th key={idx} scope="col">
-                  <div onClick={(e) => handleColumnClick(e, idx)}>
-                    {title}
-                    <MdFilterList
-                      className={
-                        clickedColumn === idx ? styles.active : styles.inactive
-                      }
-                    />
-                    {clickedColumn === idx && (
-                      <FilterDropDownModal
-                        arrayLen={columnTitle.length}
-                        index={idx}
-                      />
-                    )}
-                  </div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody ref={tbodyRef}>
-            {tableData?.map((item, idx) => (
-              <tr key={idx} className={styles.dashTableRow}>
-                <td>{item?.organisation}</td>
-                <td>{item?.username}</td>
-                <td>{item?.email}</td>
-                <td>{item?.phone}</td>
-                <td>{item?.date}</td>
-                {userStatus({ status: item.status })}
-                <td className="relative">
-                  <span onClick={() => handleRowClick(idx)}>
-                    <SlOptionsVertical className={styles.optionIcon} />
+        {isLoading ? (
+          <Loader />
+        ) : hasError.status ? (
+          <h2 className={styles.hasError}>{hasError.msg}</h2>
+        ) : userDataArr.length === 0 ? (
+          <h2 className={styles.notFound}>No Record Found!</h2>
+        ) : (
+          <>
+            {isFilter ||
+              (isSearch && (
+                <div className={styles.goBack}>
+                  <span onClick={() => navigate(`${pathname}?page=${page}`)}>
+                    <img src={arrowBackIcon} />
                   </span>
-                  {clickedRow === idx && (
-                    <UserOptionModal arrayLen={tableData.length} index={idx} />
+                  {isFilter && <h3>Showing filtered results...</h3>}
+                  {isSearch && (
+                    <h3>
+                      Search Result -{" "}
+                      <span className={styles.result}>{searchTerm}</span>
+                    </h3>
                   )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                </div>
+              ))}
+            <table ref={tableRef}>
+              <thead>
+                <tr>
+                  {columnTitle.map((title, idx) => (
+                    <th key={idx} scope="col">
+                      <div onClick={(e) => handleColumnClick(e, idx)}>
+                        {title}
+                        <MdFilterList
+                          className={
+                            clickedColumn === idx
+                              ? styles.active
+                              : styles.inactive
+                          }
+                        />
+                        {clickedColumn === idx && (
+                          <FilterDropDownModal
+                            arrayLen={columnTitle.length}
+                            index={idx}
+                            close={() => setClickedColumn(null)}
+                          />
+                        )}
+                      </div>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody ref={tbodyRef}>
+                {(isFilter || isSearch
+                  ? userFilteredDataArr
+                  : userDataArr
+                )?.map((item, idx) => (
+                  <tr key={idx} className={styles.dashTableRow}>
+                    <td>{item?.profile.organization}</td>
+                    <td>{item?.profile.username}</td>
+                    <td>{item?.profile.email}</td>
+                    <td>{item?.profile.phoneNumber}</td>
+                    <td>{formatDate(item?.profile.createdAt)}</td>
+                    {userStatus({ status: item.profile.status })}
+                    <td className="relative">
+                      <span onClick={() => handleRowClick(idx)}>
+                        <SlOptionsVertical className={styles.optionIcon} />
+                      </span>
+                      {clickedRow === idx && (
+                        <UserOptionModal
+                          username={item.profile.username}
+                          arrayLen={userDataArr.length}
+                          index={idx}
+                        />
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
       </div>
-      <Pagination
-        maxNumOfPages={maxNumOfPages}
-        currentPageNum={currentPageNum}
-        increasePageNum={increasePageNum}
-        decreasePageNum={decreasePageNum}
-      />
+      {!isLoading && !hasError.status && (
+        <Pagination
+          page={page}
+          totalResults={totalPages || 0}
+          maxVisiblePages={3}
+          onPageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 }
