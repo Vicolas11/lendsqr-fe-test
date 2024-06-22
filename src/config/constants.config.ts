12c6fd6000b7da4cc912649fe1b2225cd79e5ce9
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-22 18:03:15 +0100

    chore: remove mockAPIById key

--- src/config/constants.config.ts
+++ src/config/constants.config.ts
@@ -5,5 +5,4 @@ export const constants = {
   dev: ENV === "development",
   prod: ENV === "production",
   mockAPI: import.meta.env.VITE_APP_MOCK_API as string,
-  mockAPIById: import.meta.env.VITE_APP_MOCK_API_BY_ID as string,
 };
