67711c3d882548faa9d4b23910de2935a2e03b70
Vicolas11 <incrediblechamp1@gmail.com>
2024-06-22 17:47:24 +0100

    refactor: modified component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,57 +1,137 @@
-import { PaginationProps } from "../../interfaces/props.interfaces";
 import { arrowLeftIcon, arrowRightIcon } from "../../assets";
-import { useEffect, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
-export default function Pagination(props: PaginationProps): JSX.Element {
-  const { maxNumOfPages, currentPageNum, increasePageNum, decreasePageNum } =
-    props;
-  const [selectNumArr, setSelectNumArr] = useState<number[]>([]);
-  const isNextPage = maxNumOfPages == currentPageNum;
-  const isPrevPage = currentPageNum <= 1;
-  const arrLen = selectNumArr.length;
-  const arrayOfNum: number[] = useMemo(() => [], []);
+interface Props {
+  page: number;
+  totalResults: number;
+  onPageChange: (page: number) => void;
+  maxVisiblePages: number;
+}
 
-  const onSelectHandler = (evt: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log("value ==>", evt.target.value);
-  };
+export default function Pagination({
+  page,
+  totalResults,
+  onPageChange,
+  maxVisiblePages,
+}: Props): JSX.Element {
+  const [currentPage, setCurrentPage] = useState(page);
+  const [selectedValue, setSelectedValue] = useState(currentPage);
 
   useEffect(() => {
-    for (let i = 1; i <= maxNumOfPages; i++) {
-      arrayOfNum.push(i);
-      setSelectNumArr(arrayOfNum);
-    }
-  }, [arrayOfNum, maxNumOfPages]);
+    setCurrentPage(page);
+  }, [page]);
 
-  const numArr = [
-    ...selectNumArr.slice(0, 3),
-    "...",
-    ...selectNumArr.slice(arrLen - 3, arrLen),
-  ];
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+
+    if (totalResults <= maxVisiblePages) {
+      for (let i = 1; i <= totalResults; i++) {
+        pageNumbers.push(i);
+      }
+    } else {
+      const startPage = Math.max(
+        1,
+        currentPage - Math.floor(maxVisiblePages / 2)
+      );
+      const endPage = Math.min(totalResults, startPage + maxVisiblePages - 1);
+
+      // Ensure startPage and endPage are within bounds
+      if (startPage > 1) {
+        pageNumbers.push(1);
+        if (startPage > 2) {
+          pageNumbers.push(null);
+        }
+      }
+
+      for (let i = startPage; i <= endPage; i++) {
+        pageNumbers.push(i);
+      }
+
+      if (endPage < totalResults) {
+        if (endPage < totalResults - 1) {
+          pageNumbers.push(null);
+        }
+        if (startPage === 1) {
+          for (let i = totalResults - 1; i <= totalResults; i++) {
+            pageNumbers.push(i);
+          }
+        } else {
+          pageNumbers.push(totalResults);
+        }
+      }
+    }
+
+    return pageNumbers;
+  };
+
+  const handlePageClick = (page: number | null) => {
+    if (page !== null) {
+      setCurrentPage(page);
+      setSelectedValue(page);
+      onPageChange(page);
+    }
+  };
+
+  const selectNumArr = () => {
+    const arrNum = [];
+    for (let i = 1; i <= totalResults; i++) {
+      arrNum.push(i);
+    }
+    return arrNum;
+  };
+
+  const onSelectHandler = (evt: ChangeEvent<HTMLSelectElement>) => {
+    const value = +evt.target.value;
+    setSelectedValue(value);
+    handlePageClick(value);
+  };
 
   return (
     <div className={styles.paginationWrapper}>
       <div className={styles.left}>
         <h1>Showing</h1>
         <span>
-          <select name="org" id="org" onChange={() => onSelectHandler}>
-            {selectNumArr.map((num, idx) => (
-              <option key={`${idx}`} value={`${num}`}>
+          <select
+            name="org"
+            id="org"
+            value={selectedValue}
+            onChange={onSelectHandler}
+          >
+            {selectNumArr().map((num) => (
+              <option key={num} value={num}>
                 {num}
               </option>
             ))}
           </select>
         </span>
-        <h1>out of {maxNumOfPages}</h1>
+        <h1>out of {totalResults}</h1>
       </div>
       <div className={styles.right}>
-        <button disabled={isPrevPage} onClick={decreasePageNum}>
+        <button
+          className={styles.prevBtn}
+          onClick={() => handlePageClick(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
           <img src={arrowLeftIcon} alt="arrow-left" />
         </button>
-        {numArr?.map((num, idx) => {
-          return <h3 key={`${idx}`}>{num}</h3>;
-        })}
-        <button disabled={isNextPage} onClick={increasePageNum}>
+        {getPageNumbers().map((pageNumber, index) => (
+          <button
+            key={index}
+            className={
+              pageNumber === currentPage ? styles.active : styles.btnPaginate
+            }
+            onClick={() => handlePageClick(pageNumber)}
+            disabled={pageNumber === null}
+          >
+            {pageNumber === null ? "..." : pageNumber}
+          </button>
+        ))}
+        <button
+          className={styles.nextBtn}
+          onClick={() => handlePageClick(currentPage + 1)}
+          disabled={currentPage === totalResults}
+        >
           <img src={arrowRightIcon} alt="arrow-right" />
         </button>
       </div>
